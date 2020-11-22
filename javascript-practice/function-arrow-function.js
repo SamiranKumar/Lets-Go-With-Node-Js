770b976f604029d19ef0822d68e5036985dfec69
samirankumar11 <samirankumar11@gmail.com>
2020-11-22 14:29:42 +0600

    test normal function

---
+++ javascript-practice/function-arrow-function.js
@@ -0,0 +1,11 @@
+//node function-arrow-function.js
+
+//Function Syntax
+function sum(a, b) {
+  return a + b;
+}
+
+var value = sum(10, 15); //invokes sum function
+console.log("sum(10, 15):" + value);
+
+//========================================
